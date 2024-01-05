c7ebfa63808accdee37c99c891bca5fec9c3655a
GERARD2619-17 <gera121134@gmail.com>
2024-01-05 10:41:13 -0600

    remember

--- logearme.js
+++ logearme.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('https://cfab-190-242-151-88.ngrok-free.app/api/auth/local', {
+            const response = await fetch('http://localhost:1337/api/auth/local', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
