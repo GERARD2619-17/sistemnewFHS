b090722548ad2db031f16448ee3522ff026ae435
GERARD2619-17 <gera121134@gmail.com>
2024-01-09 23:09:23 -0600

    prueba2conpm2

--- pm2.config.js
+++ pm2.config.js
@@ -9,8 +9,8 @@
       },
     ],
   };*/
-  // pm2.config.js
-module.exports = {
+  // pm2.config.js ---- segunda configuracion de prueba
+/*module.exports = {
     apps: [
       {
         name: 'mi-strapi-app',
@@ -25,6 +25,30 @@ module.exports = {
         },
         env_production: {
           NODE_ENV: 'production',
+        },
+      },
+    ],
+  };*/
+  // pm2.config.js
+module.exports = {
+    apps: [
+      {
+        name: 'mi-strapi-app',
+        script: 'node_modules/strapi/bin/strapi.js',
+        args: 'develop',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development',
+          HOST: '0.0.0.0',
+          PORT: 1337,
+        },
+        env_production: {
+          NODE_ENV: 'production',
+          HOST: '0.0.0.0',
+          PORT: 1337,
         },
       },
     ],
